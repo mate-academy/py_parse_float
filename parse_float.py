4c517d6f987af258e95fa5a014f2f18d508e0e80
DukhDmytro <cowboybebop4991@gmail.com>
2019-11-22 11:21:48 +0200

    2 commit

--- parse_float.py
+++ parse_float.py
@@ -8,15 +8,14 @@ def parse(string: str) -> bool:
     if string.isdigit() or (string[0] == "-" and string[1:].isdigit()):
         return True
 
-    minus_count = 0
     dot_count = 0
 
     for i, char in enumerate(string):
         if not char.isdigit():
             if char == '.' and dot_count == 0:
                 dot_count += 1
-            elif i == 0 and char == '-' and minus_count == 0:
-                minus_count += 1
+            elif i == 0 and char == '-':
+                continue
             else:
                 return False
     return True
