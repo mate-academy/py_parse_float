5a57b22154634fb4bee8294b9a81ccc0b1d82f53
Dima <dima.sapsay@gmail.com>
2019-11-22 16:49:01 +0200

    first commit

--- parse_float.py
+++ parse_float.py
@@ -1,3 +1,23 @@
-def parse(s: str) -> bool:
+"""
+check a string
+"""
 
-    return False
+def parse(strg: str) -> bool:
+    """
+    func
+    """
+
+    if not strg:
+        return False
+
+    if '-' in strg[1:]:
+        return False
+
+    if strg.count('.') > 1:
+        return False
+
+    for i in strg:
+        if i.isalpha():
+            return False
+
+    return True
