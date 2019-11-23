84f35161b0a1ef74c2b3fa29f75f90b0c5c11920
d1makov <makovskyj.danylo@gmail.com>
2019-11-23 11:49:37 +0100

    fin

--- parse_float.py
+++ parse_float.py
@@ -1,3 +1,14 @@
-def parse(s: str) -> bool:
+"""
+docstring
+"""
 
-    return False
+
+def parse(abc: str) -> bool:
+    """
+
+    :param abc:
+    :return:
+    """
+    if abc.count('.') > 1 or abc.count('-') > 1 or len(abc) < 1 or any(ch.isalpha() for ch in abc):
+        return False
+    return True
