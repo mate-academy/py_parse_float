f6dfdacacc5a68eafbff838eae0098296f6363c7
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-24 19:24:33 +0200

    Resolved

--- parse_float.py
+++ parse_float.py
@@ -1,3 +1,10 @@
-def parse(s: str) -> bool:
+"""module docstring"""
+def parse(mb_float):
+    """docstring"""
+    if not mb_float or mb_float[0] not in "-.0123456789" or mb_float.count(".") > 1:
+        return False
+    for i in range(1, len(mb_float)):
+        if mb_float[i] not in ".0123456789":
+            return False
 
-    return False
+    return True
