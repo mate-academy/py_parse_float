5adfdd78b9aa2f898a9d983066a13375d861b1ca
alexander <earthhuman41@gmail.com>
2019-11-25 08:56:45 +0200

    implemented parse_float

--- parse_float.py
+++ parse_float.py
@@ -1,3 +1,29 @@
-def parse(s: str) -> bool:
+"""
+parse
+"""
+def parse(string: str) -> bool:
+    """
 
-    return False
+    :param string: string
+    :return: bool True if string is valid float number
+    """
+    if len(string) == 0:
+        return False
+
+    if string[0] == '-':
+        string = string[1:]
+
+    dot_count = 0
+
+    for char in string:
+        if char == '.':
+            if dot_count != 0:
+                return False
+
+            dot_count += 1
+            continue
+
+        if ord(char) <= 48 or ord(char) >= 57:
+            return False
+
+    return True
