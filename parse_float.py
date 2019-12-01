71c9625db94210f12f14de3ca5f522c007b704db
zaikovlad95@gmail.com <Makepeacenotwar1995>
2019-12-01 19:49:23 +0200

    Define if string is float number.

--- parse_float.py
+++ parse_float.py
@@ -1,3 +1,12 @@
-def parse(s: str) -> bool:
+"""parse module."""
+import re
 
-    return False
+
+def parse(string_input: str) -> bool:
+    """gets True or False if input is float number."""
+    if not string_input or type(string_input) is not str:
+        return False
+    if string_input.count('-') > 1 or string_input.count('.') > 1 \
+            or re.search('[a-zA-Z]', string_input):
+        return False
+    return True
