6d710ba3a771a351a8f4fd2b99986f0d98a2edce
zaikovlad95@gmail.com <Makepeacenotwar1995>
2019-12-02 23:51:03 +0200

    Get 10 of 10 by pylint.

--- parse_float.py
+++ parse_float.py
@@ -4,9 +4,15 @@ import re
 
 def parse(string_input: str) -> bool:
     """gets True or False if input is float number."""
-    if not string_input or type(string_input) is not str:
+    def is_string(initial_input: str) -> bool:
+        return not initial_input or not isinstance(initial_input, str)
+
+    def is_valid_string(some_str: str) -> bool:
+        return some_str.count('-') > 1 or some_str.count('.') > 1 \
+            or re.search('[a-zA-Z]', some_str)
+    if is_string(string_input):
         return False
-    if string_input.count('-') > 1 or string_input.count('.') > 1 \
-            or re.search('[a-zA-Z]', string_input):
+
+    if is_valid_string(string_input):
         return False
     return True
